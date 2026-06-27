f7ec22c09de01b4109f58910df79e5d1f9023c1d
Alphan Algül <alphanalgul@gmail.com>
2026-06-27 15:06:18 +0300

    Update Player.h

--- C++/GalacticBattle/Player.h
+++ C++/GalacticBattle/Player.h
@@ -1,6 +1,4 @@
-/* Alphan Algul 2584639
-I read and accept the submission rules and the extra rules specified
-in each question. This is my own work that is done by me only */
+
 #ifndef UNTITLED_PLAYER_H
 #define UNTITLED_PLAYER_H
 
